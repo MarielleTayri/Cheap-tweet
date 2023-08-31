b40fb97a457d24bb7dcff915c8964e82dc8e9b52
myriam graine <myriam.bdn@gmail.com>
2023-08-31 22:30:37 +0200

    adding delete button

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -1,10 +1,11 @@
-export function Tweet({name, content, like}) {
+export function Tweet({name, content, like, onDelete}) {
   const onLike = () => {
     console.log("LIKE", name);
   };
 
   return (
-  <div className="tweet">
+  <div className="tweet" o>
+    <button className="delete">❌</button>
     <h3>{name}</h3>
     <p>{content}</p>
     <button onClick={ () => onLike()}>{like}💙</button>
