b7a0a3e193c49a5cda0bd333624415b938a47431
myriam graine <myriam.bdn@gmail.com>
2023-08-31 17:30:31 +0200

    add id and key

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 import { Tweet } from "./Tweet";
 
-const DEFAULT_TWEET = [    {
+const DEFAULT_TWEET = [
+  {
+  id: 0,
   name:"Amel",
   content: "enfin brf",
   like:1500,
 },
 {
+  id: 1,
   name:"Yannis",
   content: "yo",
   like:500,
 },
 {
+  id: 2,
   name:"Najib",
   content: "poète",
   like:300,
 },
 {
+  id: 3,
   name:"Lila",
   content: "zarma",
   like:1000,
@@ -25,6 +30,17 @@ const DEFAULT_TWEET = [    {
 
 function App() {
   let [tweets, setTweets] = useState(DEFAULT_TWEET);
+
+  const tweetsList = tweets.map((tweet) => {
+    return (
+      <Tweet
+      key= {tweet.id}
+      name={tweet.name}
+      content={tweet.content}
+      like={tweet.like}
+      />
+    );
+  });
 
   return (
     <div>
